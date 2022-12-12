fbeab367921414e31004a546ec74d8e580146c62
Randerson Silva <randerson.ghoul@gmail.com>
2022-12-12 15:54:40 -0300

    first commit

---
+++ assets/js/scrollReveal.js
@@ -0,0 +1,20 @@
+window.sr = ScrollReveal({ reset: true })
+
+ScrollReveal().reveal('.area-1',{
+    delay:500
+});
+ScrollReveal().reveal('.area-2',{
+    delay:500
+});
+ScrollReveal().reveal('.area-3',{
+    delay:500
+});
+ScrollReveal().reveal('.area-4', {
+    delay:500
+});
+ScrollReveal().reveal('.area-5',{
+    delay:500
+});
+ScrollReveal().reveal('.area-6',{
+    delay:500
+});
